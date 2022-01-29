d761000dd34e69332735831046dd25f2200c18bb
Nicolò Bartolini <nicolobartolinii@gmail.com>
2022-01-29 12:03:35 +0100

    Tentativo di creazione solver

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-function createBoard() {
+/* function createBoard() {
     let board = [
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
@@ -10,34 +10,26 @@ function createBoard() {
         [0, 0, 0, 0, 0, 0, 0, 0, 0],
         [0, 0, 0, 0, 0, 0, 0, 0, 0]
     ];
-    /* for (let i = 0; i < 9; i += 3) {
+    for (let i = 0; i < 9; i += 3) {
         for (let j = 0; j < 9; j += 3) {
             if (i === j) {
                 fillBox(board, i, j);
             }
         }
     }
-    console.log("Completate diagonali");
     for (let i = 0; i < 9; i += 3) {
         for (let j = 0; j < 9; j += 3) {
             if (i !== j) {
                 fillBox(board, i, j);
             }
         }
-    } */
-    for (let i = 0; i < 9; i++) {
-        for (let j = 0; j < 9; j++) {
-            checkTotal(board, i, j);
-        }
     }
     return board;
 }
 
-function getNum() {
-    return Math.round((Math.random() * 8) + 1);
-}
 
-function checkCell(board, x, y, num) {
+
+function checkDiagonalBox(board, x, y, num) {
     let isValid = true;
     for (let i = 0; i < 3 && isValid; i++) {
         for (let j = 0; j < 3 && isValid; j++) {
@@ -52,12 +44,11 @@ function checkCell(board, x, y, num) {
 function checkBox(board, row, col, num) {
     let isValid = true;
     for (let i = 0; i < 3 && isValid; i++) {
-        console.log("Primo for checkBox");
         for (let j = 0; j < 3 && isValid; j++) {
-            console.log("Secondo for checkBox");
-            if (board[row - (row % 3) + i][col - (col % 3) + j] === num) {
-                isValid = false;
-            }
+            if (i !== row && j !== col)
+                if (board[row + i][col + j] === num) {
+                    isValid = false;
+                }
         }
     }
     return isValid;
@@ -66,9 +57,9 @@ function checkBox(board, row, col, num) {
 function checkRow(board, row, col, num) {
     let isValid = true;
     for (let i = 0; i < 9 && isValid; i++) {
-        console.log("for checkRow");
-        if (board[row][i] === num)
-            isValid = false;
+        if (i !== col)
+            if (board[row][i] === num)
+                isValid = false;
     }
     return isValid;
 }
@@ -76,7 +67,6 @@ function checkRow(board, row, col, num) {
 function checkColumn(board, row, col, num) {
     let isValid = true;
     for (let i = 0; i < 9 && isValid; i++) {
-        console.log("for checkColumn");
         if (i !== row)
             if (board[i][col] === num)
                 isValid = false;
@@ -84,26 +74,116 @@ function checkColumn(board, row, col, num) {
     return isValid;
 }
 
-function checkTotal(board, row, col) {
-    for (let k = 0; k < col; k++) {
-        for (let i = 0; i < 9; i++) {
-            if ((checkBox(board, row, col, i) && checkRow(board, row, col, i) && checkColumn(board, row, col, i))) {
-                board[row][col] = i;
-                return;
-            }
-        }
-        checkTotal(board, row, col - 1);
+function checkTotal(board, row, col, num) {
+    while (!(checkBox(board, row, col, num) && checkRow(board, row, col, num) && checkColumn(board, row, col, num))) {
+        num = getNum();
     }
-    checkTotal(board, row - 1, 8);
+    return num;
 }
 
 function fillBox(board, rowStart, colStart) {
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             if (rowStart === colStart)
-                board[rowStart + i][colStart + j] = checkCell(board, rowStart, colStart, getNum());
+                board[rowStart + i][colStart + j] = checkDiagonalBox(board, rowStart, colStart, getNum());
             else
                 board[rowStart + i][colStart + j] = checkTotal(board, rowStart, colStart, getNum());
         }
     }
+} */
+
+function getNum() {
+    return Math.round((Math.random() * 8) + 1);
+}
+
+function fillDiagBox(board, x, y) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            let num = getNum();
+            while (!checkBox(board, x, y, num))
+                num = getNum();
+            board[x + i][y + j] = num;
+        }
+    }
+}
+
+function checkBox(board, x, y, num) {
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            if (board[x - (x % 3) + i][y - (y % 3) + j] === num)
+                return false;
+        }
+    }
+    return true;
+}
+
+function checkRow(board, row, num) {
+    for (let i = 0; i < 9; i++) {
+        if (board[row][i] === num)
+            return false;
+    }
+    return true;
+}
+
+function checkCol(board, col, num) {
+    for (let i = 0; i < 9; i++) {
+        if (board[i][col] === num)
+            return false;
+    }
+    return true;
+}
+
+function createBoard() {
+    let board = [
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0],
+        [0, 0, 0, 0, 0, 0, 0, 0, 0]
+    ];
+    for (let i = 0; i < 9; i += 3) {
+        for (let j = 0; j < 9; j += 3) {
+            if (i === j) {
+                fillDiagBox(board, i, j);
+            }
+        }
+    }
+    /* for (let i = 0; i < 9; i++) {
+        for (let j = 0; j < 9; j++) {
+            if (board[i][j] === 0) {
+
+            }
+        }
+    } */
+
+    return board;
+}
+
+
+function solver(board, startRow = 0, startCol = 0, backtracking = false) {
+    for (let i = startRow; i < 9; i++) {
+        for (let j = i === startRow ? startCol : 0; j < 9; j++) {
+            if (board[i][j] === 0 || backtracking) {
+                let possibilities = [];
+                console.log(possibilities.length);
+                for (let k = 0; k < 9; k++) {
+                    if (checkBox(board, i, j, k) && checkRow(board, i, k) && checkCol(board, j, k))
+                        possibilities.push(k);
+                }
+                if (possibilities.length > 0)
+                    board[i][j] = possibilities[0];
+                else if (possibilities.length === 1)
+                    board[i][j] = possibilities[0];
+                else if (possibilities.length === 0) {
+                    solver(board, j === 0 ? i - 1 : i, j === 0 ? 8 : j - 1, true);
+                }
+                console.log(`Solver. [${i}][${j}]: ${possibilities}. Length: ${possibilities.length}`);
+            }
+        }
+    }
+    return board;
 }
